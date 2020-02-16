87a3a1fda0d4015be3ac847368735a37259bb931
Arseniy Obolenskiy <gooddoog@student.su>
2020-02-16 11:55:29 +0300

    [VSCODE] Optimize extensions deployment

--- update.py
+++ update.py
@@ -3,8 +3,9 @@ import subprocess
 def main():
     with open("vscode-extensions.sh", "w") as f:
         f.write("#!/bin/sh\n")
+        f.write("code \\\n")
         f.flush()
-        subprocess.call("code --list-extensions | xargs -L 1 echo code --install-extension", stdout=f, shell=True)
+        subprocess.call("code --list-extensions | xargs -L 1 -I '$' echo '--install-extension $ \\'", stdout=f, shell=True)
 
 if __name__ == "__main__":
     main()
