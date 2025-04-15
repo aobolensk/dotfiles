b37bc6ccb4318ba818c360d0df5d5e2ad9ee369a
Arseniy Obolenskiy <gooddoog@student.su>
2025-04-15 11:22:25 +0200

    [VSCODE] Update extensions list only if VS Code is installed on the system

--- update.py
+++ update.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import argparse
+import shutil
 import subprocess
 
 
@@ -10,14 +11,17 @@ Dotfiles update script.
 - gathers extensions list and updates installation scripts.
 """, formatter_class=argparse.RawTextHelpFormatter)
     _ = parser.parse_args()
-    with open("vscode-extensions.sh", "w", encoding="utf-8", newline='\n') as f:
-        f.write("#!/bin/sh\n")
-        f.write("code --force \\\n")
-        f.flush()
-        proc = subprocess.run("code --list-extensions", stdout=subprocess.PIPE, shell=True, check=True)
-        out = proc.stdout.decode("utf-8").strip().split('\n')
-        for line in out:
-            f.write("--install-extension {} \\\n".format(line))
+    if shutil.which("code"):
+        with open("vscode-extensions.sh", "w", encoding="utf-8", newline='\n') as f:
+            f.write("#!/bin/sh\n")
+            f.write("code --force \\\n")
+            f.flush()
+            proc = subprocess.run("code --list-extensions", stdout=subprocess.PIPE, shell=True, check=True)
+            out = proc.stdout.decode("utf-8").strip().split('\n')
+            for line in out:
+                f.write("--install-extension {} \\\n".format(line))
+    else:
+        print("Warning: VSCode not found in PATH. Skipping extension update.")
 
 
 if __name__ == "__main__":
